4b4fcc0666811183510df011f834c42a728441b5
MartinRB45534 <martin.rialhe-badet@etu.univ-savoie.fr>
2022-04-14 09:07:10 +0200

    Perfectionnisme, quand tu nous tiens...

--- affichage/animation.py
+++ affichage/animation.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
+import sys
 
 from circuit.circuit import Circuit
 from circuit.spline_generator import cree_spline
 from circuit.spline_opener import readfile
 from affichage.camera import Camera, Camera_premiere_personne, Camera_troisieme_personne, Camera_fixe_dirigee
 
-circuit = Circuit(cree_spline(readfile('rollercoaster.mycustomfileextension')))
+if len(sys.argv) <= 1:
+    exit("Indiquer le fichier du roller coaster !")
+
+pts_ctrl,cams = readfile(sys.argv[1])
+circuit = Circuit(cree_spline(pts_ctrl))
 circuit.cabine.vitesse=3
 
 fig = plt.figure()
@@ -21,7 +26,7 @@ ax2.grid(False)
 camera_pp = Camera_premiere_personne(circuit) #La caméra qui suit la cabine de près
 ax3 = fig.add_subplot(1, 4, 3, projection='3d')
 ax3.grid(False)
-camera_fd = Camera_fixe_dirigee(circuit,[np.array([4,-4,0]),np.array([15,-5,0]),np.array([9,5,0]),np.array([3,5,0])]) #Les caméras qui tournent pour suivre la cabine
+camera_fd = Camera_fixe_dirigee(circuit,cams) #Les caméras qui tournent pour suivre la cabine
 ax4 = fig.add_subplot(1, 4, 4, projection='3d')
 ax4.grid(False)
 
--- circuit/spline_opener.py
+++ circuit/spline_opener.py
@@ -17,8 +17,23 @@ def readfile(filename) :
   except:
     raise Not_enough_lines()
 
+  f.readline()
+  nb_cams = int(f.readline())
+  try:
+    assert(nb_cams>0)
+  except:
+    raise Not_enough_cams(nb_cams)
+  cams=np.zeros((nb_cams,3),np.double)
+  try:
+    for i in range(nb_cams) :
+      f.readline() #Devrait être une ligne vide. Rajouter un test.
+      for j in range(3):
+          cams[i,j]=np.double(f.readline())
+  except:
+    raise Not_enough_lines()
+
   f.close() 
-  return points
+  return points, cams
 
 class File_opening_error(Exception):
   pass
@@ -29,6 +44,11 @@ class Not_enough_points(File_opening_error):
     self.message = f"Il faut au moins 4 points pour dessiner une spline, le fichier n'en a que {nb_points} !"
     super().__init__(self.message)
 
+class Not_enough_cams(File_opening_error):
+  def __init__(self, nb_cams):
+    self.nb_points = nb_cams
+    self.message = f"Il faut au moins 1 caméra fixe ! Le fichier n'en a que {nb_cams} !"
+    super().__init__(self.message)
 class Not_enough_lines(File_opening_error):
   def __init__(self):
     super().__init__("File is too short !")
